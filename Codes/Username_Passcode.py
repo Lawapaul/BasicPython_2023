d67d7619be1d9e362239e6d40a54ef6568a889af
Lawapaul <harshitshekhawat501@gmail.com>
2023-09-01 23:21:14 +0530

    Add new File

---
+++ Codes/Username_Passcode.py
@@ -0,0 +1,40 @@
+
+print()
+print("-"*50)
+
+print("\n                 X Y Z     A G E N C Y             ")
+
+print()
+print("-"*50)
+
+while True:
+    a=input("Enter Username: ")
+    if a=="admin":
+        b=input("Enter Passcode: ")
+        if b=="test":
+            
+            """Hello Everyone, I hope you find my module interesting and helpful. 
+               Start writing your code from here.......
+            """
+            
+            
+            
+    else:
+        print("Wrong Username: ") 
+       
+        print("\nDo you wish to continue (y/n): ") 
+        a=input()
+        if a=='n' or a=='N':
+            print("Thankyou!,Have a great day :) ") 
+            break
+        else:
+            continue
+        
+        
+          
+    
+            
+            
+            
+        
+        
