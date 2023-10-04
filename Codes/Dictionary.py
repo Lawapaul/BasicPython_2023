c5671ba632cd394ab6f53e038b2d1e497a9aac06
Lawapaul <harshitshekhawat501@gmail.com>
2023-10-04 23:38:20 +0530

    Pyramid File Added

---
+++ Codes/Dictionary.py
@@ -0,0 +1,9 @@
+drinks = [{"type": "water","calories": 0,"number_consumed": 7,"caffeinated": False },
+    { "type": "orange juice","calories": 220,"number_consumed": 4,"caffeinated": False },
+    {"type": "gatorade","calories": 140,"number_consumed": 1,"caffeinated": False},
+    { "type": "cappuccino", "calories": 350,"number_consumed": 2,"caffeinated": True},
+    {"type": "hot tea","calories": 5,"number_consumed": 3, "caffeinated": True}]
+
+for i in drinks():
+    print(i)
+    
