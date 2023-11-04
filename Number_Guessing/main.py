a84b2f3b4206371346fdcd12b7f972ec6d76b2dd
Lawapaul <harshitshekhawat501@gmail.com>
2023-11-04 10:26:02 +0530

    New Game Added

--- Number_Guessing/main.py
+++ Number_Guessing/main.py
@@ -2,33 +2,96 @@
 import random
 
 
-a=input("\nEnter your Name: ")
-b=input("Enter your Surname: ")
-print("-"*50)
-print("\n               D I C E    G A M E          ")
+a=input("\nEnter First and Middle Name(if any): ")
+b=input("Enter Surname: ")
+c=input("Enter Username: ")
+print("-"*60)
+print("\n               T I M E   P A S S    G A M E          ")
 print()
-print("-"*50)
+print("-"*60)
 
+def main(a,b,c):
+    print()
+    print()
+    print("*","Welcome",a,b,"*")
+    print("*","Username",c,"*")
+    print()
+    c=list(map(int,input("Enter range in format ( Lower Number (space) Highest Number ): ").split()))
+    print("Range:",c[0],"-",c[1])
+    ch=input("Do you wish to Re-Enter(y/n) : ")
+    if ch=='y':
+        d=list(map(int,input("Enter range in format ( Lower Number (space) Highest Number ): ").split()))    
+        print("Range:",d[0],"-",d[1])
+        c=d
 
-print()
-print()
-print("*","Welcome",a,b,"*")
-print()
-c=list(map(int,input("Enter range in format ( Lower Number (space) Highest Number ): ").split()))
-print("Range:",c[0],"-",c[1])
-ch=input("Do you wish to Re-Enter(y/n) : ")
-if ch=='y':
-    d=list(map(int,input("Enter range in format ( Lower Number (space) Highest Number ): ").split()))    
-    print("Range:",d[0],"-",d[1])
-    c=d
+    main=random.randint(c[0],c[1])   
+    print(main)
+    print()
+    print("Random Number Generating......")
+    print("Random Number Generated!!!")
+    print()
+    inpu=int(input("Enter Number: "))
 
-main=random.randint(c[0],c[1])   
-print()
-print("Random Number Generating......")
-print("Random Number Generated!!!")
-print()
-inpu=int(input("Enter Number: "))
+    if inpu>c[0] and inpu<c[1]:
+        if inpu==main:
+            print("Congratulation!!!! Guessed in First Attempt")
+        else:
+            print("Wrong!!!! Try Again (3 Chances Left).")
+            abc=int(input("Enter Number: "))
+            if abc==main:
+                print("Congratulation. You WIN!!!")
+            elif abc>0:
+                efg=int(input("Guessed Too High. Try again (2 Chances left): "))
+                if efg==main:
+                        print("Congratulations! You guessed right ^_^")
+                elif efg>main:
+                        chance_2=int(input("Guessed too high again (Last Chance): "))
+                        if chance_2==main:
+                            print("Congratulations!!! You guessed right ^_^")
+                        else:
+                            print("Failed. Better Luck next time.")
+                            print("Answer was:",main)
+            
+                elif efg<main:
+                        chance_2=int(input("Guessed Too low. Try again (Last Chance): "))
+                        if chance_2==main:
+                            print("Congratulations!!! You guessed Right ^_^")
+                        else:
+                            print("Failed. Better Luck Next time.")
+                            print("Answer was:",main)
+            elif abc<0:
+                efg=int(input("Guessed Too Low. Try again (2 Chances left): "))
+                if efg==main:
+                    print("Congratulations! You guessed right ^_^")
+                elif efg>main:
+                    chance_2=int(input("Guessed Too high again (Last Chance): "))
+                    if chance_2==main:
+                        print("Congratulations!!! You guessed right ^_^")
+                    else:
+                        print("Failed. Better Luck next time.")
+                        print("Answer was:",main)
+            
+                elif efg<main:
+                    chance_2=int(input("Guessed Too low. Try again (Last Chance): "))
+                    if chance_2==main:
+                        print("Congratulations!!! You guessed Right ^_^")
+                    else:
+                        print("Failed. Better Luck Next time.")
+                        print("Answer was:",main)
+    else:
+        print("Number out of Range!!")     
     
+main(a,b,c)
+while True:
+    ch=input("Do you wish to continue (y/n): ")
+    if ch=="n" or ch=="N":
+        print("Thankyou. Have a great day ahead ^_^")    
+        break
+    else:
+        main(a,b,c)
+
+
+            
 
     
     
---
+++ Number_Guessing/readme.txt
@@ -0,0 +1,12 @@
+H E L L O   E V E R Y O N E !!
+
+---------------------------------
+
+Welcome to my page! 
+Hope you all are doing well. I am exicted to announce that I have created my very first own game named 
+"TIME-PASS" which asks user to guess a number between the range given by them.
+
+
+---------------------------------
+
+**I am Begineer and uploading it for my concern. Do not entertain it has a final executable programe**
