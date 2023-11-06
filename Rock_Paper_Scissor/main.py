ca71a58de2ff076ac5890541c991127306b62f53
Lawapaul <harshitshekhawat501@gmail.com>
2023-11-06 07:45:15 +0530

    Bug Fix

--- Rock_Paper_Scissor/main.py
+++ Rock_Paper_Scissor/main.py
@@ -16,39 +16,45 @@ def main():
         main=random.randint(1,4)
         compu=""
         if main==3:
-            compu="Rock"
+            compu="rock"
         elif main==2:
-            compu="Paper"
+            compu="paper"
         elif main==1:
-            compu="Scissor"
-        inpu=input("\n1. Rock\n2. Paper\n3. Scissor\nEnter Your Choice: ")
-        print(main)
-        if compu.lower()==inpu.lower():
-            print("Tie. Try Again!")
-        elif compu=="Rock":
-            if compu=="Rock" and inpu.lower=="paper":
+            compu="scissor"
+        inpu=input("\nRock\nPaper\nScissor\nEnter Your Choice: ")
+        inpu.lower()
+        if compu=="rock":
+            if compu=="rock" and inpu.lower=="paper":
                 print("You Won. Congratulations!!!")    
                 count+=1
-            elif compu=="Rock" and inpu.lower=="scissor":
+            elif compu=="rock" and inpu.lower=="scissor":
                 print("You Lost. Try Again!")
                 count1+=1
+            elif compu=="rock" and inpu.lower()=="rock":
+                print("Tie. Try Again")
             else:
-                print("Invalid Input.")
-        elif compu=="Paper":
-            if compu=="Paper" and inpu=="rock":
+                print("Invalid Input. Try again!")
+        elif compu=="paper":
+            if compu=="paper" and inpu=="rock":
                 print("You Loss. Better Luck Next Time!!!")
                 count1+=1
-            elif compu=="Paper" and inpu=="scissor":
+            elif compu=="paper" and inpu=="scissor":
                 print("You won. Congratulations!!!")
                 count+=1
+            elif compu=="paper" and inpu=="paper":
+                print("Tie. Try again!")
             else:
-                print("Invalid Input.")
-        elif compu=="Scissor":
-            if compu=="Scissor" and inpu=="rock":
+                print("Invalid Input. Try again!")
+        elif compu=="scissor":
+            if compu=="scissor" and inpu=="rock":
                 print("You Won. Congratulations!!!!")
                 count=+1
-            elif compu=="Scissor" and inpu=="paper":
+            elif compu=="scissor" and inpu=="paper":
                 print("You loss. Better Luck next Time!!!")
+            elif compu=="scissor" and inpu=="scissor":
+                print("Tie. Try again!")
+            else:
+                print("Invalid Input. Try Again!")
     if a>1:            
         if count==count1:
             print("\nMatch Tied. Well played!!!")
