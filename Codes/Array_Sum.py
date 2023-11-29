4a5af431c8adfcc9342bae5f62844a93aceec30b
Lawapaul <harshitshekhawat501@gmail.com>
2023-11-29 17:01:30 +0530

    New File Added

--- Codes/Array_Sum.py
+++ Codes/Array_Sum.py
@@ -7,8 +7,12 @@ for i in range(a):
     lst.append(int(input()))
 flag=False
 count=0
+count1=0
+for i in lst:
+    if i==0:
+        count+=1
 for i in range(len(lst)):
-    if len(lst)==0 or len(lst)==1:
+    if len(lst)==0 or lst[i]==0:
         flag==False
     elif sum(lst[0:i])==sum(lst[i+1:len(lst)]):
         count=i
